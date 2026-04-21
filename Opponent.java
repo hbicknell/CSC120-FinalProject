8177861d93b0a3473fddb103fdb6cdd4d44d69f2
Hayden Bicknell <hbicknell@smith.edu>
2026-04-21 16:47:01 -0400

    oops

--- Opponent.java
+++ Opponent.java
@@ -2,12 +2,13 @@ public class Opponent {
     private String name; 
     private String opponentVisualDescription;
     private int opponentHealth;
+    private boolean opponentAlive;
 
     /*default constructor */
     public Opponent(String name, String description, int health){
         this.name = name;
         this.opponentVisualDescription = description;
         this.opponentHealth = health;
-        
+        this.opponentAlive = true;
     }
 }
